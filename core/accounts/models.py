95ec073d328e810f8c1df15f16915e6708c8ba12
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2024-09-25 23:40:59 +0330

    Added accounts and blog app models.py codes

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import (BaseUserManager,AbstractBaseUser,PermissionMixin)
+
 
 # Create your models here.
+class User (AbstractBaseUser,PermissionMixin):
+    username=models.CharField(max_length=250,unique=True)
+    email=models.EmailField(max_length=255,unique=True)
+    is_staff=models.BooleanField(default=False)
+    is_active=models.BooleanField(default=True)
+    # is_verified=models.BooleanField(default=False)
+    REQUIRED_FIELDS=[]
+
+    created_date=models.DateField(auto_now_add=True)
+    updated_date=models.DateField(auto_now=True)
+
+
+    def __str__(self):
+        return self.username
--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    """
+    this is a shape of our posts
+    """
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    image=models.ImageField()
+    title=models.CharField(max_length=30)
+    content=models.TextField(max_length=250)
+    status=models.BooleanField(default=False)
+    created_date=models.DateField(auto_now_add=True)
+    updated_date=models.DateField(auto_now=True)
+    published_date=models.DateTimeField(null=True,blank=True)
+
+
+    def __str__(self):
+        return self.title
+
