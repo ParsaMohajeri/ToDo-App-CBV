3b8c78837f36e7587232400bbe130c110fa562ea
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2024-10-16 23:55:36 +0330

    commenting

--- core/blog/apps.py
+++ core/blog/apps.py
@@ -3,5 +3,8 @@ from django.apps import AppConfig
 
 
 class BlogConfig(AppConfig):
+    """
+    this is a class to make config settings
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog'
