ff15ffaa592ea49b368e2443d8a9de7b18bd6a84
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2024-10-17 20:58:17 +0330

    commenting

--- core/accounts/apps.py
+++ core/accounts/apps.py
@@ -3,5 +3,8 @@ from django.apps import AppConfig
 
 
 class AccountsConfig(AppConfig):
+    """
+    this is a class to make config settings
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
