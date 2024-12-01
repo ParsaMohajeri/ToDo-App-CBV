cadc60c8cb881750ec776b36fa04736a9617dea5
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2024-12-01 18:21:16 +0330

    Update views.py

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,6 +1,10 @@
 # import part
 from django.shortcuts import render
-
+from django.views.generic.base import TemplateView,RedirectView
+from .models import Post
+from django.views.generic import ListView,DetailView,FormView,CreateView,UpdateView,DeleteView
+from .forms import PostForm
+from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
 
 # _____________________________________________________________________
 # Create your views here.
