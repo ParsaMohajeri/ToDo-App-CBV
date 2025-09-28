6adc117149de52f8e77c4ac8c41c3ce5e45c37f5
Mohammad Parsa Mohajeri <parsamohajeri1188@gmail.com>
2025-09-28 23:19:26 +0330

    fetching information from openweather

--- core/work/api/v1/views.py
+++ core/work/api/v1/views.py
@@ -22,6 +22,11 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from .paginations import DefaultPagination
 from django.shortcuts import redirect
+import requests
+from django.http import JsonResponse
+from django.conf import settings
+
+
 
 data = {"id": 1, "title": "hello"}
 
@@ -41,3 +46,45 @@ class TaskModelViewSet(viewsets.ModelViewSet):
         if user.is_authenticated:
             return Task.objects.filter(author=user)
         return Task.objects.none()
+
+
+
+
+
+
+
+
+import requests
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.conf import settings
+
+
+class WeatherViewSet(viewsets.ViewSet):
+
+
+    @action(detail=False, methods=['get'])
+    def current(self, request):
+        city = request.query_params.get("city", "Tehran")
+        api_key = settings.OPENWEATHER_API_KEY 
+        url = "https://api.openweathermap.org/data/2.5/weather"
+
+        params = {
+            "q": city,
+            "appid": api_key,
+            "units": "metric",
+        }
+
+        response = requests.get(url, params=params)
+        data = response.json()
+
+        if response.status_code != 200:
+            return Response({"error": data.get("message", "something went wrong")}, status=response.status_code)
+
+        result = {
+            "city": data["name"],
+            "temperature": data["main"]["temp"],
+            "description": data["weather"][0]["description"],
+        }
+        return Response(result)
